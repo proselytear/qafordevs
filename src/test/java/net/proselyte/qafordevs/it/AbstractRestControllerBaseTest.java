fe5c35f315c74756661a8769e2520d4c7a1f07ea
Maxim Orlov <orl2011.2011@gmail.com>
2024-05-16 22:02:53 +0300

    Not working version

--- src/test/java/net/proselyte/qafordevs/it/AbstractRestControllerBaseTest.java
+++ src/test/java/net/proselyte/qafordevs/it/AbstractRestControllerBaseTest.java
@@ -1,5 +1,6 @@
 package net.proselyte.qafordevs.it;
 
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
---
+++ src/test/java/net/proselyte/qafordevs/it/ItAnotherOneTests.java
@@ -0,0 +1,22 @@
+package net.proselyte.qafordevs.it;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ItAnotherOneTests extends AbstractRestControllerBaseTest {
+    private static final Logger log = LoggerFactory.getLogger("abobus");
+    @Test
+    public void bimbimbambam() {
+        log.info("Doing bimbimbambam test");
+    }
+
+}
